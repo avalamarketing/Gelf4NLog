3a14cffda3b92996645b15bd0a84e16e1bac6703
Ryan Bartsch <rbartsch@homestart.com.au>
2016-11-02 01:37:27 +1030

    fixed existing unit tests, which were severly broken

---
+++ UnitTest/GelfConverterTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net;
+using Gelf4NLog.Target;
+using NLog;
+using NUnit.Framework;
+
+namespace Gelf4NLog.UnitTest
+{
+    [TestFixture(Category = "GelfConverter")]
+    public class GelfConverterTests
+    {
+        [Test]
+        public void ShouldCreateGelfJsonCorrectly()
+        {
+            var timestamp = DateTime.Now;
+            var logEvent = new LogEventInfo
+            {
+                Message = "Test Log Message",
+                Level = LogLevel.Info,
+                TimeStamp = timestamp,
+                LoggerName = "GelfConverterTestLogger"
+            };
+            logEvent.Properties.Add("customproperty1", "customvalue1");
+            logEvent.Properties.Add("customproperty2", "customvalue2");
+
+            var jsonObject = new GelfConverter().GetGelfJson(logEvent, "TestFacility", "DEV");
+
+            Assert.IsNotNull(jsonObject);
+            Assert.AreEqual("1.1", jsonObject.Value<string>("version"));
+            Assert.AreEqual(Dns.GetHostName(), jsonObject.Value<string>("host"));
+            Assert.AreEqual("Test Log Message", jsonObject.Value<string>("short_message"));
+            Assert.AreEqual("Test Log Message", jsonObject.Value<string>("full_message"));
+            Assert.AreEqual(timestamp, jsonObject.Value<DateTime>("timestamp"));
+            Assert.AreEqual(6, jsonObject.Value<int>("level"));
+            Assert.AreEqual("TestFacility", jsonObject.Value<string>("_facility"));
+            Assert.AreEqual("DEV", jsonObject.Value<string>("_environment"));
+
+            Assert.AreEqual("customvalue1", jsonObject.Value<string>("_customproperty1"));
+            Assert.AreEqual("customvalue2", jsonObject.Value<string>("_customproperty2"));
+            Assert.AreEqual("GelfConverterTestLogger", jsonObject.Value<string>("_LoggerName"));
+            Assert.AreEqual("Info", jsonObject.Value<string>("_LogLevelName"));
+
+            //make sure that there are no other junk in there
+            Assert.AreEqual(12, jsonObject.Count);
+        }
+
+        [Test]
+        public void ShouldHandleExceptionsCorrectly()
+        {
+            var logEvent = new LogEventInfo
+            {
+                Message = "Test Message",
+                Exception = new DivideByZeroException("div by 0")
+            };
+
+            var jsonObject = new GelfConverter().GetGelfJson(logEvent, "TestFacility", "DEV");
+
+            Assert.IsNotNull(jsonObject);
+            Assert.AreEqual("Test Message", jsonObject.Value<string>("short_message"));
+            Assert.AreEqual("Test Message", jsonObject.Value<string>("full_message"));
+            Assert.AreEqual(3, jsonObject.Value<int>("level"));
+            Assert.AreEqual("TestFacility", jsonObject.Value<string>("_facility"));
+            Assert.AreEqual("DEV", jsonObject.Value<string>("_environment"));
+            Assert.AreEqual(null, jsonObject.Value<string>("_ExceptionSource"));
+            Assert.AreEqual("div by 0", jsonObject.Value<string>("_ExceptionMessage"));
+            Assert.AreEqual(null, jsonObject.Value<string>("_StackTrace"));
+            Assert.AreEqual(null, jsonObject.Value<string>("_LoggerName"));
+        }
+
+        [Test]
+        public void ShouldHandleLongMessageCorrectly()
+        {
+            var logEvent = new LogEventInfo
+            {
+                //The first 300 chars of lorem ipsum...
+                Message =
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum est in est cursus vitae pellentesque felis lobortis. Donec a orci quis ante viverra eleifend ac et quam. Donec imperdiet libero ut justo tincidunt non tristique mauris gravida. Fusce sapien eros, tincidunt a placerat nullam."
+            };
+
+            var jsonObject = new GelfConverter().GetGelfJson(logEvent, "TestFacility", "DEV");
+
+            Assert.IsNotNull(jsonObject);
+            Assert.AreEqual(250, jsonObject.Value<string>("short_message").Length);
+            Assert.AreEqual(300, jsonObject.Value<string>("full_message").Length);
+        }
+
+        [Test]
+        public void ShouldHandlePropertyCalledIdProperly()
+        {
+            var logEvent = new LogEventInfo {Message = "Test"};
+            logEvent.Properties.Add("Id", "not_important");
+
+            var jsonObject = new GelfConverter().GetGelfJson(logEvent, "TestFacility", "DEV");
+
+            Assert.IsNotNull(jsonObject);
+            Assert.IsNull(jsonObject["_id"]);
+            Assert.AreEqual("not_important", jsonObject.Value<string>("_id_"));
+        }
+    }
+}
---
+++ UnitTest/UdpTransportTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using Gelf4NLog.Target;
+using Gelf4NLog.UnitTest.Resources;
+using Moq;
+using Newtonsoft.Json.Linq;
+using NLog;
+using NUnit.Framework;
+
+namespace Gelf4NLog.UnitTest
+{
+    [TestFixture]
+    public class UdpTransportTests
+    {
+        [Test]
+        public void ShouldSendLongUdpMessage()
+        {
+            var jsonObject = new JObject();
+            var message = ResourceHelper.GetResource("LongMessage.txt").ReadToEnd();
+
+            jsonObject.Add("full_message", JToken.FromObject(message));
+
+            var converter = new Mock<IConverter>();
+            converter.Setup(c => c.GetGelfJson(It.IsAny<LogEventInfo>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(jsonObject)
+                .Verifiable();
+            var transportClient = new Mock<ITransportClient>();
+            transportClient.Setup(t => t.Send(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IPEndPoint>())).Verifiable();
+
+            var transport = new UdpTransport(transportClient.Object);
+
+            var target = new NLogTarget(transport, converter.Object) {Endpoint = "udp://localhost:12201"};
+            target.WriteLogEventInfo(new LogEventInfo());
+
+            transportClient.Verify(t => t.Send(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IPEndPoint>()),
+                Times.Exactly(2));
+            converter.Verify(c => c.GetGelfJson(It.IsAny<LogEventInfo>(), It.IsAny<string>(), It.IsAny<string>()),
+                Times.Once());
+        }
+
+        [Test]
+        public void ShouldSendShortUdpMessage()
+        {
+            var transportClient = new Mock<ITransportClient>();
+            var transport = new UdpTransport(transportClient.Object);
+            var converter = new Mock<IConverter>();
+            converter.Setup(c => c.GetGelfJson(It.IsAny<LogEventInfo>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new JObject());
+
+            var target = new NLogTarget(transport, converter.Object) {Endpoint = "udp://localhost:12201"};
+            var logEventInfo = new LogEventInfo {Message = "Test Message"};
+
+            target.WriteLogEventInfo(logEventInfo);
+
+            transportClient.Verify(t => t.Send(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IPEndPoint>()),
+                Times.Once());
+            converter.Verify(c => c.GetGelfJson(It.IsAny<LogEventInfo>(), It.IsAny<string>(), It.IsAny<string>()),
+                Times.Once());
+        }
+    }
+}
